625017d43b151b71738f97103d80bcf1fc61fece
Madhuri Dere <madhuridere26@gmail.com>
2021-05-22 21:11:21 +0530

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,40 @@
+
+from django.contrib.auth.models import User, Group
+from client.models import Client, Project
+from rest_framework import serializers
+from user.serializers import *
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+# class ClientSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Client
+#         field = ('client_name')
+
+
+class ClientSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        field = '__all__'
+
+
+class RegisterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
